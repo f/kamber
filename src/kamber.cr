06f9f0b8dffe05f56c5f74ab5ecb2ede85aaac3b
Fatih Kadir Akın <fakin@protel.com.tr>
2015-12-07 23:33:27 +0200

    stylings

--- src/kamber.cr
+++ src/kamber.cr
@@ -5,17 +5,18 @@ require "yaml"
 require "kemal"
 require "./config"
 
-$posts = YAML.load_all File.read("./posts/posts.yml")
-
 module Kamber
+
   get "/" do
+    posts = YAML.load_all File.read("./posts/posts.yml")
     render "src/kamber/views/index.ecr"
   end
 
   get "/:post" do |env|
+    posts = YAML.load_all File.read("./posts/posts.yml")
     post = {} of String => String
     contents = ""
-    $posts.each do |_post|
+    posts.each do |_post|
       _post = _post as Hash
       if _post.has_key? "file"
         if (_post["file"] as String).ends_with?("#{env.params["post"]}.md" as String)
@@ -25,5 +26,10 @@ module Kamber
       end
     end
     render "src/kamber/views/post.ecr"
+  end
+
+  get "/style/:path" do |env|
+    env.content_type = "text/css"
+    File.read "src/kamber/static/css/#{env.params["path"]}"
   end
 end
