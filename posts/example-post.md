32f6bac1a89d7b836c489620679d4d2e29cc2632
Fatih Kadir Akın <fakin@protel.com.tr>
2015-12-07 22:57:11 +0200

    Make it run.

---
+++ posts/example-post.md
@@ -0,0 +1,16 @@
+# An h1 header
+
+Paragraphs are separated by a blank line.
+
+2nd paragraph. *Italic*, **bold**, and `monospace`. Itemized lists
+look like:
+
+  * this one
+  * that one
+  * the other one
+
+```
+function () {
+  
+}
+```
--- src/kamber.cr
+++ src/kamber.cr
@@ -1,13 +1,29 @@
 require "./kamber/*"
 
 require "markdown"
+require "yaml"
 require "kemal"
 require "./config"
 
+$posts = YAML.load_all File.read("./posts/posts.yml")
+
 module Kamber
   get "/" do
-    post_title = "hey"
-    post_text = File.read "posts/example-post.md"
     render "src/kamber/views/index.ecr"
+  end
+
+  get "/:post" do |env|
+    post = {} of String => String
+    contents = ""
+    $posts.each do |_post|
+      _post = _post as Hash
+      if _post.has_key? "file"
+        if (_post["file"] as String).ends_with?("#{env.params["post"]}.md" as String)
+          post = _post
+          contents = File.read(_post["file"] as String)
+        end
+      end
+    end
+    render "src/kamber/views/post.ecr"
   end
 end
