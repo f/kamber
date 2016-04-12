66fd2d40307a0ec9c7a001b7bb4cb3658dca2182
Ender Ahmet Yurt <enderyurt@gmail.com>
2016-04-13 01:22:20 +0300

    Update syntax.

--- src/kamber.cr
+++ src/kamber.cr
@@ -5,13 +5,14 @@ require "yaml"
 require "kemal"
 require "./config"
 
-$posts = YAML.load_all File.read("./posts/posts.yml")
+$posts = YAML.parse File.read("./posts/posts.yml")
 
 def post_item(file)
   post = {} of String => String
   contents = ""
   $posts.each do |_post|
-    _post = _post as Hash
+    puts _post
+    _post = _post.as_h as Hash
     if _post.has_key? "file"
       if (_post["file"] as String).ends_with?("#{file}.md" as String)
         post = _post
@@ -23,36 +24,34 @@ def post_item(file)
 end
 
 module Kamber
-
   get "/" do
     theme_index
   end
-
   get "/style/:path" do |env|
-    env.content_type = "text/css"
-    File.read theme_style(env.params["path"])
+    env.response.content_type = "text/css"
+    File.read theme_style(env.params.query["path"])
   end
 
   get "/script/:path" do |env|
-    env.content_type = "application/javascript"
-    File.read theme_script(env.params["path"])
+    env.response.content_type = "application/javascript"
+    File.read theme_script(env.params.query["path"])
   end
 
   get "/:post" do |env|
-    post_item(env.params["post"])
+    post_item(env.params.query["post"])
   end
 
   get "/:category/:post" do |env|
-    category = env.params["category"] as String
-    post = env.params["post"] as String
+    category = env.params.query["category"] as String
+    post = env.params.query["post"] as String
     post_item(category + "/" + post)
   end
 
   get "/:category/:subcategory/:post" do |env|
-    category = env.params["category"] as String
-    subcategory = env.params["subcategory"] as String
-    post_name = env.params["post"] as String
+    category = env.params.query["category"] as String
+    subcategory = env.params.query["subcategory"] as String
+    post_name = env.params.query["post"] as String
     post_item(category + "/" + subcategory + "/" + post_name)
   end
-
+  Kemal.run
 end
