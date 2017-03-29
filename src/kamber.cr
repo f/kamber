4e2374a50ae25b4d36ac4beff0fd6330672135ad
Tommaso Patrizi <tpatrizi@makeplan.it>
2017-03-29 16:23:46 +0200

    kamber working with latest crystal and kemal

--- src/kamber.cr
+++ src/kamber.cr
@@ -5,28 +5,33 @@ require "yaml"
 require "kemal"
 require "./config"
 
-$posts = YAML.parse File.read("./posts/posts.yml")
+module Kamber
+  @@posts = YAML.parse_all File.read("./posts/posts.yml")
 
-def post_item(file)
-  post = {} of String => String
-  contents = ""
-  $posts.each do |_post|
-    puts _post
-    _post = _post.as_h as Hash
-    if _post.has_key? "file"
-      if (_post["file"] as String).ends_with?("#{file}.md" as String)
-        post = _post
-        contents = File.read(_post["file"] as String)
+  def self.posts
+    @@posts
+  end
+
+  def self.post_item(file)
+    post = {} of String => String
+    contents = ""
+    @@posts.each do |_post|
+      puts _post
+      _post = _post.as_h.as(Hash)
+      if _post.has_key? "file"
+        if (_post["file"].as(String).ends_with?("#{file}.md".as(String)))
+          post = _post
+          contents = File.read(_post["file"].as(String))
+        end
       end
     end
+    theme_item(post, contents)
   end
-  theme_item(post, contents)
-end
 
-module Kamber
   get "/" do
     theme_index
   end
+
   get "/style/:path" do |env|
     env.response.content_type = "text/css"
     File.read theme_style(env.params.query["path"])
@@ -38,19 +43,19 @@ module Kamber
   end
 
   get "/:post" do |env|
-    post_item(env.params.query["post"])
+    post_item(env.params.url["post"])
   end
 
-  get "/:category/:post" do |env|
-    category = env.params.query["category"] as String
-    post = env.params.query["post"] as String
+  get "/categories/:category/:post" do |env|
+    category = env.params.url["category"].as(String)
+    post = env.params.url["post"].as(String)
     post_item(category + "/" + post)
   end
 
-  get "/:category/:subcategory/:post" do |env|
-    category = env.params.query["category"] as String
-    subcategory = env.params.query["subcategory"] as String
-    post_name = env.params.query["post"] as String
+  get "/subcategories/:category/:subcategory/:post" do |env|
+    category = env.params.url["category"].as(String)
+    subcategory = env.params.url["subcategory"].as(String)
+    post_name = env.params.url["post"].as(String)
     post_item(category + "/" + subcategory + "/" + post_name)
   end
   Kemal.run
