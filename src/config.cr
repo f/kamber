9f12949a42f1ea4cc0bbc7bd243111e8f6e3dd10
Corban Raun <craun@instructure.com>
2017-10-28 10:29:27 -0600

    Whitespace removal

--- src/config.cr
+++ src/config.cr
@@ -3,7 +3,7 @@ module BlogWide
   @@BLOG_DESC = "Tommaso Patrizi personal views on tech and universe"
   @@GOOGLE_ANALYTICS = "UA-32444807-1"
 
-  def self.title 
+  def self.title
     @@BLOG_TITLE
   end
 
